f9f361ab910d0bbb5af76db13a2a4eeea04a99af
zxc3buttons <tokare2002@gmail.com>
2022-01-23 21:18:21 +0300

    added init and destroy methods to ClassicalMusic bean

--- src/main/java/ru/tokarev/springcourse/ClassicalMusic.java
+++ src/main/java/ru/tokarev/springcourse/ClassicalMusic.java
@@ -2,6 +2,14 @@ package ru.tokarev.springcourse;
 
 public class ClassicalMusic implements Music {
 
+    public void doInitMethod() {
+        System.out.println("Initialisation!");
+    }
+
+    public void doDestroyMethod() {
+        System.out.println("Destruction!");
+    }
+
     @Override
     public String getSong() {
         return "Hungarian Rhapsody instance";
